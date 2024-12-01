f25ef32c95975a8764df223ce7f3485d6b6af99f
user <user@172.17.181.179>
2024-12-02 01:26:07 +0800

    this is my answer for lab6 q1

---
+++ lab6/src/lab6/q1.java
@@ -0,0 +1,31 @@
+package lab6;
+import java.util.Scanner;
+public class q1 {
+
+    public static void main(String[] args) {
+        
+        int integerNum , numberSum = 0, countPositive = 0, countNegative = 0, count = 0;
+        double average;
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Enter integer number : ");
+        integerNum = s.nextInt();
+        
+        while (integerNum !=0){
+        if (integerNum > 0 ){
+            countPositive = countPositive + 1;}
+        else 
+        {countNegative = countNegative + 1;}
+        
+        numberSum = numberSum + integerNum;
+        count++;
+        System.out.print("\nEnter integer number : ");
+        integerNum = s.nextInt();
+    }
+        average = numberSum/count;
+        System.out.println("total number positive: " + countPositive);
+        System.out.println("total number negative: " + countNegative);
+        System.out.println("total from all integer number: " + numberSum);
+        System.out.println("average :" + average);
+        }
+}

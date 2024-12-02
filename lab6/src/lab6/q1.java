c7020fac020e1f79d12ca4d4695b9ccfb8f631b9
user <user@172.17.181.179>
2024-12-02 14:09:06 +0800

    buat sedikit pembetulan

--- lab6/src/lab6/q1.java
+++ lab6/src/lab6/q1.java
@@ -1,39 +1,40 @@
 package lab6;
+
 import java.util.Scanner;
+
 public class q1 {
 
     public static void main(String[] args) {
-        
-        int integerNum = 0 ;
-        int numberSum = 0 ;
-        int countPositive = 0 ;
-        int countNegative = 0 ;
+
+        int integerNum = 0;
+        int numberSum = 0;
+        int countPositive = 0;
+        int countNegative = 0;
         int count = 0;
         double average;
         Scanner s = new Scanner(System.in);
-        
+
         System.out.print("Enter integer number : ");
         integerNum = s.nextInt();
-        
-        while (integerNum !=0){
-        if (integerNum > 0 ){
-            countPositive = countPositive + 1;}
-        else 
-        {countNegative = countNegative + 1;}
-        
-        numberSum = numberSum + integerNum;
-        count++;
-    
-        System.out.print("\nEnter integer number : ");
-        integerNum = s.nextInt();
-        
-        average = numberSum/count;
-        System.out.println("total number positive: " + countPositive);
-        System.out.println("total number negative: " + countNegative);
-        System.out.println("total from all integer number: " + numberSum);
-        System.out.println("average :" + average);
-        break;
-        
+
+        while (integerNum != 0) {
+            if (integerNum > 0) {
+                countPositive = countPositive + 1;
+            } else {
+                countNegative = countNegative + 1;
+            }
+
+            numberSum = numberSum + integerNum;
+            count++;
+
+            System.out.print("Enter integer number : ");
+            integerNum = s.nextInt();
+
+            average = numberSum / count;
+            System.out.println("\ntotal number positive: " + countPositive);
+            System.out.println("total number negative: " + countNegative);
+            System.out.println("total from all integer number: " + numberSum);
+            System.out.println("average :" + average);
         }
-}
+    }
 }
